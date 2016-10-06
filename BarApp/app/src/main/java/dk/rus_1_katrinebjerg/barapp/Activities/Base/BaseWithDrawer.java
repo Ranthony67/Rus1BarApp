ccc82abe608d766251f5d1e829306519fca361e2
Ranthony67 <rasmusanthonynielsen@gmail.com>
2016-10-06 20:37:36 +0200

    Updated activities to extends BaseWithDrawer.

---
+++ BarApp/app/src/main/java/dk/rus_1_katrinebjerg/barapp/Activities/Base/BaseWithDrawer.java
@@ -0,0 +1,12 @@
+package dk.rus_1_katrinebjerg.barapp.Activities.Base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rasmus on 06/10/2016.
+ */
+
+public class BaseWithDrawer extends AppCompatActivity {
+
+
+}
---
+++ BarApp/app/src/main/java/dk/rus_1_katrinebjerg/barapp/Activities/StatusActivity.java
@@ -0,0 +1,15 @@
+package dk.rus_1_katrinebjerg.barapp.Activities;
+
+import android.os.Bundle;
+
+import dk.rus_1_katrinebjerg.barapp.Activities.Base.BaseWithDrawer;
+import dk.rus_1_katrinebjerg.barapp.R;
+
+public class StatusActivity extends BaseWithDrawer {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+}
