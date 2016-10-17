584d189ff007afce0a0997de416c7808fb29e69a
Ranthony67 <rasmusanthonynielsen@gmail.com>
2016-10-17 20:17:50 +0200

    Created Drawer holding one item.

--- BarApp/app/src/main/java/dk/rus_1_katrinebjerg/barapp/Activities/Base/BaseWithDrawer.java
+++ BarApp/app/src/main/java/dk/rus_1_katrinebjerg/barapp/Activities/Base/BaseWithDrawer.java
@@ -1,12 +1,26 @@
 package dk.rus_1_katrinebjerg.barapp.Activities.Base;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-/**
- * Created by Rasmus on 06/10/2016.
- */
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+
+import dk.rus_1_katrinebjerg.barapp.R;
 
 public class BaseWithDrawer extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        new DrawerBuilder()
+                .withActivity(this)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withIdentifier(1).withName(R.string.mainActivity)
+                )
+                .withToolbar(toolbar)
+                .build();
+    }
 }
