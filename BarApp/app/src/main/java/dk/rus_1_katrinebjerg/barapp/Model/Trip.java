4162bfe5d047e874f92a9845541603c6d99cff36
Ranthony67 <rasmusanthonynielsen@gmail.com>
2017-01-03 18:09:24 +0100

    Added Realm and updated the models.

--- BarApp/app/src/main/java/dk/rus_1_katrinebjerg/barapp/Model/Trip.java
+++ BarApp/app/src/main/java/dk/rus_1_katrinebjerg/barapp/Model/Trip.java
@@ -2,13 +2,10 @@ package dk.rus_1_katrinebjerg.barapp.Model;
 
 import io.realm.RealmList;
 import io.realm.RealmObject;
-import io.realm.annotations.PrimaryKey;
 
-public class BarItem extends RealmObject{
-    @PrimaryKey
-    public int id;
+public class Trip extends RealmObject {
     public String name;
-    public double price;
 
     public RealmList<Tutor> Tutors;
+    public RealmList<BarItem> BarItems;
 }
--- BarApp/app/src/main/java/dk/rus_1_katrinebjerg/barapp/Model/Tutor.java
+++ BarApp/app/src/main/java/dk/rus_1_katrinebjerg/barapp/Model/Tutor.java
@@ -1,37 +1,11 @@
 package dk.rus_1_katrinebjerg.barapp.Model;
 
-import android.content.ContentValues;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import io.realm.RealmList;
+import io.realm.RealmObject;
 
-public class Tutor extends BaseModel{
-    public static final String TABLE_NAME = "tutor";
-
+public class Tutor extends RealmObject{
     public int id;
     public String name;
     public String streetName;
-
-
-
-    @Override
-    public ContentValues getContentValues() {
-        return null;
-    }
-
-    @Override
-    public void populateFromCursor(Cursor cursor) {
-
-    }
-
-    public static void createTable(SQLiteDatabase db) {
-        String CreateTable = "CREATE TABLE " + TABLE_NAME +
-                "(" +
-                "id INTEGER PRIMARY KEY," +
-                "name TEXT" +
-                "streetName TEXT" +
-                "spendAmount REAL" +
-                ")";
-
-        db.execSQL(CreateTable);
-    }
+    public RealmList<BarItem> BarItemsBought;
 }
