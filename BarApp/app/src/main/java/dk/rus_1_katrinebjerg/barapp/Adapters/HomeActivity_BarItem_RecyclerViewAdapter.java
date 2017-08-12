46a8ea5a767a54a5101df159f08e277e205ca3af
Jan Rasmussen <201370226@post.au.dk>
2017-08-12 10:56:27 +0200

    WIP

---
+++ BarApp/app/src/main/java/dk/rus_1_katrinebjerg/barapp/Adapters/HomeActivity_BarItem_RecyclerViewAdapter.java
@@ -0,0 +1,8 @@
+package dk.rus_1_katrinebjerg.barapp.Adapters;
+
+/**
+ * Created by Jan on 11-08-2017.
+ */
+
+public class HomeActivity_BarItem_RecyclerViewAdapter {
+}
---
+++ BarApp/app/src/main/java/dk/rus_1_katrinebjerg/barapp/Adapters/RecyclerViewOnTuchListener.java
@@ -0,0 +1,8 @@
+package dk.rus_1_katrinebjerg.barapp.Adapters;
+
+/**
+ * Created by Jan on 12-08-2017.
+ */
+
+class RecyclerViewOnTuchListener {
+}
